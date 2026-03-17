c790a2b9f40c9a13d51ad3f5ac2c10a117d10aa2
camillel29-cpu <camille.l29@aluno.ifsc.edu.br>
2026-03-17 13:20:51 +0000

    adicionar animação e controle

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 450,
+  width: 400,
+  height: 225,
   parent: "game-container",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: true,
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  }
 };
 
 export default config;
--- scene0.js
+++ scene0.js
@@ -1,24 +1,101 @@
-class sescene0 extends Phaser.Scene {
+class scene0 extends Phaser.Scene {
   constructor() {
     super("scene0");
+
+    this.threshold = 0.1;
+    this.speed = 100;
+    this.direction = undefined;
   }
 
-preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 'assets/dude.png',
-    {
-      frameWidth: 32,
-      frameHeight: 48,
+  preload() {
+    this.load.spritesheet("nv", "assets/personagens/nv.png", {
+      frameWidth: 16,
+      frameHeight: 16,
     });
 
+    this.load.plugin(
+      "rexvirtualjoystickplugin",
+      "./rexvirtualjoystickplugin.min.js",
+      true,
+    );
+  }
+
+  create() {
+    this.anims.create({
+      key: "walk-up",
+      frames: this.anims.generateFrameNumbers("nv", { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-left",
+      frames: this.anims.generateFrameNumbers("nv", { start: 8, end: 15 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-right",
+      frames: this.anims.generateFrameNumbers("nv", { start: 24, end: 31 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-down",
+      frames: this.anims.generateFrameNumbers("nv", { start: 16, end: 23 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.nv = this.physics.add.sprite(200, 100, "nv", 20);
+
+    this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
+      x: 50,
+      y: 200,
+      radius: 25,
+      base: this.add.circle(0, 0, 25, 0xcccccc),
+      thumb: this.add.circle(0, 0, 12, 0x666666),
+    });
+
+    this.joystick.on("update", () => {
+      const angle = Phaser.Math.DegToRad(this.joystick.angle);
+      const force = this.joystick.force;
+
+      if (force > this.threshold) {
+        this.direction = new Phaser.Math.Vector2(
+          Math.cos(angle),
+          Math.sin(angle),
+        ).normalize();
+      }
+
+      if (this.joystick.force > 0) {
+        this.nv.setVelocity(
+          this.direction.x * this.speed,
+          this.direction.y * this.speed
+        );
+
+        switch (true) {
+          case this.joystick.angle >= -135 && this.joystick.angle < -45:
+            this.nv.anims.play("walk-up", true);
+            break;
+          case this.joystick.angle >= -45 && this.joystick.angle < 45:
+            this.nv.anims.play("walk-right", true);
+            break;
+          case this.joystick.angle >= 45 && this.joystick.angle < 135:
+            this.nv.anims.play("walk-down", true);
+            break;
+          case this.joystick.angle >= 135 || this.joystick.angle < -135:
+            this.nv.anims.play("walk-left", true);
+            break;
+        }
+      } else {
+        this.nv.setVelocity(0, 0);
+        this.nv.anims.stop();
+      }
+    });
+  }
 }
 
-  create() { }
-
-  update() { }
-}
-
-export default sescene0;
+export default scene0;
